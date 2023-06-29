296a8dd6eb3cceec3a0ba4c284deb1178da539fc
alfredrodriguez98 <alfredrodriguez1998@outlook.com>
2023-06-29 14:58:02 +0530

    Added a mock aggregator contract for testing purpose

---
+++ contracts/MockAggregatorV3.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title Mock Aggregator V3 Interface
+/// @author Alfred Rodriguez
+/// @notice This contract is used to mock the Aggregator V3 Interface for testing purposes only
+
+interface AggregatorV3Interface {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80,
+            int256,
+            uint256,
+            uint256,
+            uint80
+        );
+}
+
+contract MockAggregatorV3Interface is AggregatorV3Interface {
+    MockAggregatorV3Interface internal priceFeed;
+
+    uint80 private roundId;
+    int256 private price;
+    uint256 private updatedAt;
+
+    constructor() {
+        roundId = 1;
+        price = 30000000; // Setting an initial price for testing
+        updatedAt = block.timestamp;
+    }
+
+    /// @notice This function is used to set the price for the mock aggregator contract
+    /// @param _price The price to set for the mock aggregator contract
+    /// @dev This function is used for testing purposes only
+
+    function setPrice(int256 _price) external {
+        require(_price > 0, "Price cannot be zero");
+
+        price = _price;
+        updatedAt = block.timestamp;
+        roundId++;
+    }
+
+    /// @notice This function is used to set the aggregator address for the mock aggregator contract
+    /// @param aggregatorAddress The address of the aggregator contract to set for the mock aggregator contract testing
+    /// @dev This function is used for testing purposes only
+
+    function setAggregatorAddress(address aggregatorAddress) public {
+        require(
+            aggregatorAddress != address(0),
+            "Aggregator address cannot be zero address"
+        );
+        require(
+            aggregatorAddress != address(this),
+            "Aggregator address cannot be this contract address"
+        );
+
+        priceFeed = MockAggregatorV3Interface(aggregatorAddress);
+    }
+
+    /// @notice This function is used to get the latest price from the aggregator contract
+    /// @dev This function is used for testing purposes only
+
+    function latestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80,
+            int256,
+            uint256,
+            uint256,
+            uint80
+        )
+    {
+        return (roundId, price, updatedAt, updatedAt, roundId);
+    }
+}
