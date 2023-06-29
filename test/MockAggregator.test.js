046c5479168b55ff49564113b26e0e1b2d600ddc
alfredrodriguez98 <alfredrodriguez1998@outlook.com>
2023-06-29 20:12:22 +0530

    Added additional TCs

--- test/MockAggregator.test.js
+++ test/MockAggregator.test.js
@@ -5,6 +5,7 @@ const { BigNumber } = require("ethers");
 describe("Checking the functionalities of Aggregator contract using MockAggregator", async () => {
   let stablecoin;
   let mockAggregator;
+  let accounts;
 
   before(async () => {
     accounts = await ethers.getSigners(1);
@@ -19,14 +20,22 @@ describe("Checking the functionalities of Aggregator contract using MockAggregat
     await stablecoin.deployed();
   });
 
+  it("Should have correct name and symbol of token from the constructor argument", async function () {
+    expect(await stablecoin.name()).to.equal("nUSD");
+    expect(await stablecoin.symbol()).to.equal("NUSD");
+  });
+
   it("Should return the price of the stablecoin", async () => {
-    const mockPrice = 100016352;
+    const mockPrice = 100074652;
 
     await mockAggregator.setPrice(mockPrice);
     const latestPrice = await stablecoin.getLatestPrice();
 
-    expect(latestPrice).to.equal(mockPrice);
+    expect(latestPrice).to.be.equal(mockPrice);
   });
 
-  
+   it("Should get the latest price and not be zero", async function () {
+     const price = await stablecoin.getLatestPrice();
+     expect(price).to.not.equal(0);
+   });
 });
