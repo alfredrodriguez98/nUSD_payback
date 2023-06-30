633db9d79908e3f9b36a4252cd5af7fd1280d582
alfredrodriguez98 <alfredrodriguez1998@outlook.com>
2023-06-30 09:13:32 +0530

    Added MATIC url

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 module.exports = {
+  solidity: "0.8.18",
   networks: {
     hardhat: {
-      forking: {
-        url: "https://polygon-mumbai.g.alchemy.com/v2/5TG6upjsaz2IKmFo29-cKxNUDlCFoZ51",
-      },
+      // forking: {
+      //   url: "https://polygon-mumbai.g.alchemy.com/v2/5TG6upjsaz2IKmFo29-cKxNUDlCFoZ51",
+      // },
+    },
+    MATIC_MUMBAI: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/5TG6upjsaz2IKmFo29-cKxNUDlCFoZ51",
+      accounts: [process.env.PRIVATE_KEY],
+      gas: "auto",
+      gasPrice: "auto",
     },
   },
-  solidity: "0.8.18",
+  
 };
