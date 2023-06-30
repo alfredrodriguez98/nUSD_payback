afd19bcb99fcc985a66cc0f7e2eab9cd39f2f41c
alfredrodriguez98 <alfredrodriguez1998@outlook.com>
2023-06-30 06:32:53 +0530

    Forked polygon mumbai testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://polygon-mumbai.g.alchemy.com/v2/5TG6upjsaz2IKmFo29-cKxNUDlCFoZ51",
+      },
+    },
+  },
   solidity: "0.8.18",
 };
